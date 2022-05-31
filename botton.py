1891455a196b46ee7c69b5032503aa147461f21e
Teddyhill <98024074+Teddyhill@users.noreply.github.com>
2022-05-31 13:27:02 +0300

    code update

--- botton.py
+++ botton.py
@@ -7,14 +7,10 @@ import random
 WINDOW_W = 1000
 WINDOW_H = 641
 WINDOW_SIZE = (WINDOW_W, WINDOW_H)
-BLACK = (0, 0, 0)
-score = 0
+
 pygame.init()
 
-font = pygame.font.SysFont("Arial", 50, bold = True, italic = True)
-Press_text = font.render("PRESS", True, BLACK)
-dont_Press_text = font.render("DONT PRESS", True, BLACK)
-scor_text = font.render("SCORE: " + str(score), False, BLACK)
+
 
 
 
@@ -26,7 +22,7 @@ bg = pygame.image.load("spongback.jpg")
 bg = pygame.transform.scale(bg,(WINDOW_W,WINDOW_H))
 
 Rbotton = pygame.image.load("botton.png")
-Rbotton =  pygame.transform.scale(Rbotton,(300,300))
+Rbotton =  pygame.transform.scale(Rbotton,(235,235))
 Gbotton = pygame.image.load("Gbotton.png")
 Gbotton =  pygame.transform.scale(Gbotton,(215,215))
 gameover = pygame.image.load("gameover.png")
@@ -36,7 +32,9 @@ gameover =  pygame.transform.scale(gameover,(800,500))
 time = 0
 
 
-
+score = 0
+tedfont = pygame.font.SysFont("Comic Sans MS" , 30)
+scor_text = tedfont.render("SCORE: " + str(score), True, (202,225,255))
 
 
 play = True
@@ -44,26 +42,25 @@ is_red = False
 switch_time = random.randint(100, 500)
 while play:
     screen.blit(bg,(0,0))
+    screen.blit(scor_text, (410, 100))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             play = False
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE and not is_red:
-                score += 1
-                scor_text = font.render("SCORE: " + str(score), False, BLACK)
-                print(score)
+                pass
             if event.key == pygame.K_SPACE and is_red:
-                score = 0
-                scor_text = font.render("SCORE: " + str(score), False, BLACK)
+                
                 screen.blit(gameover,(100,70))
                 pygame.display.flip()
                 pygame.time.delay(5000)
                 pygame.event.get()
     time = time + 1
     if is_red:
-        screen.blit(Rbotton,(350,170))
+        screen.blit(Rbotton,(389,200))
     else:
         screen.blit(Gbotton,(395,210))
+        
 
     if time>=switch_time:
         is_red = not is_red
